eb52160f37e348c01dfb8b9ce985d0dedab7efdf
frantic <frantic.git@gmail.com>
2019-06-29 17:46:16 +0400

    Первый рабочий вариант, но есть что править

--- app/src/main/java/com/frantic/kotcalc/MainActivity.kt
+++ app/src/main/java/com/frantic/kotcalc/MainActivity.kt
@@ -7,10 +7,12 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity(){
 
-    val tag = "calc_log"
+    private val tag = "calc_log"
 
     lateinit var tvPreview:TextView
     lateinit var tvResult:TextView
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity(){
 
     var result:Double = 0.0
     var isOperation:Boolean = false
+
+    var isDivByZero:Boolean = false
 
     var operations:ArrayList<String> = ArrayList()
     var nums:ArrayList<Double> = ArrayList()
@@ -39,30 +43,16 @@ class MainActivity : AppCompatActivity(){
     //обработчик чисел
     fun onNumberClick(v:View){
         var numberName = (v as Button).text.toString()
-        when(v.id){
-            R.id.btnDot->{
-                if(num.isEmpty()){
-                    numberName = "0$numberName"
-                    num.append(numberName)
-                    operation.append(numberName)
-                    isOperation = false
-                    return
-                }
-                if(num.contains("-") && num.length == 1){
-                    numberName = "0$numberName"
-                    num.append(numberName)
-                    operation.append(numberName)
-                    isOperation = false
-                    return
-                }
-            }
-            else->{
-                num.append(numberName)
-                operation.append(numberName)
-                isOperation = false
-            }
+
+        if(v.id == R.id.btnDot){
+            if(num.isEmpty()) numberName = "0$numberName"
+            if(num.contains("-") && num.length == 1) numberName = "0$numberName"
+            if(num.contains("."))return
         }
 
+        num.append(numberName)
+        operation.append(numberName)
+        isOperation = false
         tvResult.text = operation
     }
 
@@ -76,7 +66,7 @@ class MainActivity : AppCompatActivity(){
                 calcEqually(operatorName)
             }
             R.id.btnDel->{
-
+                calcDelete(operatorName)
             }
             R.id.btnPlus->{
                 calcPlus(operatorName)
@@ -85,14 +75,17 @@ class MainActivity : AppCompatActivity(){
                 calcMinus(operatorName)
             }
             R.id.btnMult->{
-
+                calcMult(operatorName)
             }
             R.id.btnDiv->{
-
+                calcDiv(operatorName)
             }
         }
-
-        tvResult.text = operation
+        if(isDivByZero){
+            tvResult.text = getString(R.string.dividedByZero)
+            isDivByZero = false
+            calcClear()
+        }else tvResult.text = operation
     }
 
     private fun calcMinus(operatorName:String){
@@ -149,40 +142,25 @@ class MainActivity : AppCompatActivity(){
             }
         }
         //если последний оператор не равно, последнее событие "число"
-        val curDigit = num.toString().toDouble()
-        when(lastOperation.toString()){
-            "+"->result = result + curDigit
-            "-"->result = result - curDigit
-            "*"->result = result * curDigit
-            ":"->if (curDigit == 0.0){
-                    Log.d(tag, "Divided by zero!!")
-                    return
-                }else result = result / curDigit
-        }
+        calcResult()
 
+        isOperation = true
         lastOperation.clear()
-        lastOperation.append("=")
+        lastOperation.append(operatorName)
         num.clear()
-        num.append(operatorName)
 
         operation.clear()
-        operation.append(result.toString())
+        operation.append(takeDoubleWithoutTail(result))
         operation.append(operatorName)
     }
 
     private fun calcPlus(operatorName:String){
-        //если последний оператор равно, представление числа содержит "+" и больше ничего
-        //if(lastOperation.toString() == "=" && num.contains(operatorName) && num.length==1)return
-
-        //если последний оператор равно и представление числа пустое
         if(lastOperation.toString() == "=" && num.isEmpty())return
-        //если последний оператор равно, представление числа содержит тольо "-"
         if(lastOperation.toString() == "=" && num.contains("-") && num.length==1){
             num.clear()
             operation.deleteCharAt(operation.length-1)
             return
         }
-        //если последний оператор равно, представление числа содержит "+" и еще числа
         if(lastOperation.toString() == "=" && num.isNotEmpty()){
             lastOperation.clear()
             lastOperation.append(operatorName)
@@ -209,29 +187,155 @@ class MainActivity : AppCompatActivity(){
             }
         }
         //если последний оператор не равно, последнее событие "число"
-        val curDigit = num.toString().toDouble()
-        when(lastOperation.toString()){
-            "+"->result = result + curDigit
-            "-"->result = result - curDigit
-            "*"->result = result * curDigit
-            ":"->if (curDigit == 0.0){
-                Log.d(tag, "Divided by zero!!")
-                return
-            }else result = result / curDigit
-        }
+        calcResult()
 
+        isOperation = true
         lastOperation.clear()
-        lastOperation.append("=")
+        lastOperation.append(operatorName)
         num.clear()
-        num.append(operatorName)
 
         operation.clear()
-        operation.append(result.toString())
+        operation.append(takeDoubleWithoutTail(result))
+        operation.append(operatorName)
+    }
+
+    private fun calcMult(operatorName: String){
+        if(lastOperation.toString() == "=" && num.isEmpty())return
+        if(lastOperation.toString() == "=" && num.contains("-") && num.length==1){
+            num.clear()
+            operation.deleteCharAt(operation.length-1)
+            return
+        }
+        if(lastOperation.toString() == "=" && num.isNotEmpty()){
+            lastOperation.clear()
+            lastOperation.append(operatorName)
+            isOperation = true
+
+            result = num.toString().toDouble()
+            num.clear()
+
+            operation.append(operatorName)
+            return
+        }
+        //если последний оператор не равно, последнее событие "операция"
+        if(isOperation){
+            when(lastOperation.toString()){
+                operatorName->return
+                else->{
+                    lastOperation.clear()
+                    lastOperation.append(operatorName)
+
+                    operation.deleteCharAt(operation.length-1)
+                    operation.append(operatorName)
+                    return
+                }
+            }
+        }
+        calcResult()
+
+        isOperation = true
+        lastOperation.clear()
+        lastOperation.append(operatorName)
+        num.clear()
+
+        operation.clear()
+        operation.append(takeDoubleWithoutTail(result))
+        operation.append(operatorName)
+    }
+
+    private fun calcDiv(operatorName: String){
+        if(lastOperation.toString() == "=" && num.isEmpty())return
+        if(lastOperation.toString() == "=" && num.contains("-") && num.length==1){
+            num.clear()
+            operation.deleteCharAt(operation.length-1)
+            return
+        }
+        if(lastOperation.toString() == "=" && num.isNotEmpty()){
+            lastOperation.clear()
+            lastOperation.append(operatorName)
+            isOperation = true
+
+            result = num.toString().toDouble()
+            num.clear()
+
+            operation.append(operatorName)
+            return
+        }
+        //если последний оператор не равно, последнее событие "операция"
+        if(isOperation){
+            when(lastOperation.toString()){
+                operatorName->return
+                else->{
+                    lastOperation.clear()
+                    lastOperation.append(operatorName)
+
+                    operation.deleteCharAt(operation.length-1)
+                    operation.append(operatorName)
+                    return
+                }
+            }
+        }
+        calcResult()
+
+        isOperation = true
+        lastOperation.clear()
+        lastOperation.append(operatorName)
+        num.clear()
+
+        operation.clear()
+        operation.append(takeDoubleWithoutTail(result))
         operation.append(operatorName)
     }
 
     private fun calcEqually(operatorName: String){
+        //если последний оператор равно и последнее действие число
+        if(lastOperation.toString() == operatorName && !isOperation)return
+        //если последний оператор не равно, последнее событие "число"
+        if(num.isNotEmpty())calcResult()
 
+        isOperation = false
+        lastOperation.clear()
+        lastOperation.append(operatorName)
+        num.clear()
+        num.append(takeDoubleWithoutTail(result))
+        operation.clear()
+        operation.append(num)
+    }
+
+    private fun calcDelete(operatorName: String){
+        calcClear()
+    }
+
+    fun takeDoubleWithoutTail(res:Double):String{
+        result = Formatter().format("%.10f",res).toString().replace(",",".").toDouble()
+        val s = result.toString()
+        val ms = s.split(".")
+        val curTail = "0.${ms[ms.size-1]}"
+        if(curTail.toDouble() == 0.0)return ms[0]
+        return s
+    }
+
+    fun calcResult(){
+        val curDigit = num.toString().toDouble()
+        when(lastOperation.toString()){
+            "+"-> result += curDigit
+            "-"->result -= curDigit
+            "*"->result *= curDigit
+            ":"->if (curDigit == 0.0){
+                Log.d(tag, "Divided by zero!!")
+                isDivByZero = true
+                return
+            }else result /= curDigit
+        }
+    }
+
+    private fun calcClear(){
+        isOperation=false
+        lastOperation.clear()
+        lastOperation.append("=")
+        operation.clear()
+        num.clear()
+        result=0.0
     }
 }
 
--- app/src/test/java/com/frantic/kotcalc/ExampleUnitTest.kt
+++ app/src/test/java/com/frantic/kotcalc/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.frantic.kotcalc
 
+import android.util.Log
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,38 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun takeDoubleWithoutTail_isCorrect(){
+        val activity = MainActivity()
+        val a:Double = 0.1
+        val b:Double = 0.2
+        val c:Double = a + b
+        assertEquals("0.3", activity.takeDoubleWithoutTail(c))
+    }
+    @Test
+    fun calcResult_isCorrect(){
+        val activity = MainActivity()
+        activity.result = 1.0
+        activity.num.append("2")
+        activity.lastOperation.append("+")
+        activity.calcResult()
+        assertEquals(3.0, activity.result,0.0000000001)
+
+        activity.result = 2.0
+        activity.num.clear()
+        activity.num.append("2")
+        activity.lastOperation.clear()
+        activity.lastOperation.append("*")
+        activity.calcResult()
+        assertEquals(4.0, activity.result,0.0000000001)
+
+        activity.result = 4.0
+        activity.num.clear()
+        activity.num.append("2")
+        activity.lastOperation.clear()
+        activity.lastOperation.append("/")
+        activity.calcResult()
+        assertEquals(4.0, activity.result,0.0000000001)
     }
 }
