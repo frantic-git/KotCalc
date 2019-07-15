1d3087cada18504d3fe2c051d72b3770e286c8bd
frantic <frantic.git@gmail.com>
2019-07-15 10:40:16 +0400

    FoxDAO и DBEntity

--- app/src/main/java/com/frantic/kotcalc/data/db/dao/FoxDAO.kt
+++ app/src/main/java/com/frantic/kotcalc/data/db/dao/FoxDAO.kt
@@ -1,4 +1,18 @@
 package com.frantic.kotcalc.data.db.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.frantic.kotcalc.data.db.Tables
+import com.frantic.kotcalc.data.db.entity.FoxEntity
+
+@Dao
 interface FoxDAO {
+
+    @Insert
+    fun insertFox(fox: FoxEntity)
+
+    @Query("select * from ${Tables.FOX_TABLE}")
+    fun getAllFox(): List<FoxEntity>
+
 }
--- app/src/main/java/com/frantic/kotcalc/data/db/entity/DbEntity.kt
+++ app/src/main/java/com/frantic/kotcalc/data/db/entity/DbEntity.kt
@@ -1,2 +1,13 @@
 package com.frantic.kotcalc.data.db.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.frantic.kotcalc.data.db.Fields
+import com.frantic.kotcalc.data.db.Tables
+
+@Entity(tableName = Tables.FOX_TABLE)
+class FoxEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = Fields.FOX_NAME_FIELD) val foxName: String
+)
